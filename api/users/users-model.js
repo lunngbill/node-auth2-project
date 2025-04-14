36a3a3cc3e0c37d4cf0dddb0b307a6be3e8c0362
Lun Ng <lunngbill@gmail.com>
2025-04-14 00:55:10 -0700

    done

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,7 +3,7 @@ const db = require('../../data/db-config.js');
 function find() {
     return db('users as u')
       .join('roles as r', 'u.role_id', 'r.role_id')
-      .select('u.user_id', 'u.username', 'r.username')
+      .select('user_id', 'username', 'role_name')
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
@@ -25,9 +25,9 @@ function find() {
 
 function findBy(filter) {
   return db('users as u')
-  .where(filter)
-  .join('roles as r', 'u.role_id', 'r.role_id')
-  .select('u.user_id', 'u.username', 'r.username')
+    .join('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'password', 'role_name' )
+    .where(filter)
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -45,6 +45,10 @@ function findBy(filter) {
 }
 
 function findById(user_id) {
+  return db('users as u')
+  .join('roles as r', 'u.role_id', 'r.role_id')
+  .select('user_id', 'username', 'role_name' )
+  .where("u.user_id", user_id).first()
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
